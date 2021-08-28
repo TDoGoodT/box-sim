bbe5c0dd9a75f3e807881a4368781830619fe6ff
Snir <bachar.snir@gmail.com>
2021-08-28 17:16:12 +0300

    working

--- box_old.py
+++ box_old.py
@@ -1,30 +1,33 @@
 import itertools as itr
 import numpy as np
 from scipy.spatial.transform import Rotation as R
-B = [[1,0,0],[0,1,0],[0,0,1]]
-rots = [0,90,180,270]
+
+B = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+rots = [0, 90, 180, 270]
+
+
 class Box:
     def __init__(self, def_arr):
         self.def_arr = def_arr
-        self.body = np.array([[0,0,0] for i in range(27)])
+        self.body = np.array([[0, 0, 0] for i in range(27)])
         self.solution = None
 
     def solve(self):
         c = 0
-        options = itr.product([0,1,2,3],repeat=25)
+        options = itr.product([0, 1, 2, 3], repeat=25)
         for op in options:
-            dir = np.array([1,0,0])
+            dir = np.array([1, 0, 0])
             for idx, x in enumerate(self.def_arr, start=1):
                 if x == 0:
-                    self.body[idx] = self.body[idx-1] + dir
+                    self.body[idx] = self.body[idx - 1] + dir
                     if any([t > 2 for t in self.body[idx]]):
                         c += 1
                         print(f"Bad {c}")
                         break
                 elif x == 1:
                     for v in B:
-                        if np.dot(dir,v) == 0:
-                            break 
+                        if np.dot(dir, v) == 0:
+                            break
                     rotation = op[idx]
                     rotation_radians = np.radians(rotation)
                     rotation_axis = dir
@@ -32,7 +35,7 @@ class Box:
                     rotation_vector = rotation_radians * rotation_axis
                     rotation = R.from_rotvec(rotation_vector)
                     dir = rotation.apply(v)
-                    self.body[idx] = self.body[idx-1] + dir
+                    self.body[idx] = self.body[idx - 1] + dir
 
                     if any([t > 2 for t in self.body[idx]]):
                         c += 1
@@ -40,5 +43,3 @@ class Box:
                         break
         print(self.body)
         return
-
-
--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import sys
 
 import numpy
 from box import Box
-def_arr = numpy.array([1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1])
+from box import plot_option
+
+def_arr = numpy.array(
+    [0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0],int
+)
+
+
 def main():
     x = Box(def_arr)
-    x.solve()
-    
-if __name__=="__main__":
+    body, option = x.solve()
+    plot_option(body,option)
+
+
+if __name__ == "__main__":
     sys.exit(main())
